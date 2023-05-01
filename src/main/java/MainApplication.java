fc986a81869d8449de9931e1bb755cddc3da23f7
Artem Kostromin <artkostromin89@gmail.com>
2023-05-01 14:56:16 +0300

    еще правки

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -2,5 +2,7 @@ public class MainApplication {
     public static void main (String[] args) {
         System.out.println("HelloWord");
         System.out.println("111111");
+        System.out.println("222222");
+        System.out.println("3333333");
     }
 }
